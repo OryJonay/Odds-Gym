a3ff2c3021a48b8d738b3cc71ceda4689ae0aac7
Ory Jonay <ojonay@infinidat.com>
2020-03-10 11:43:55 +0200

    Daily Bets Environment
    Implementing a betting environment where the bets are grouped by their
    date.
    Observations are now a group of betting odds and the actions are
    a list of actions (an action per betting odd).

--- tests/test_three_way_env.py
+++ tests/test_three_way_env.py
@@ -4,14 +4,15 @@ from gym.spaces import Box
 
 def test_attributes(three_way_env):
     assert three_way_env.action_space.n == 2 ** 3
-    assert three_way_env.observation_space == Box(low=1., high=float('Inf'), shape=(3,))
+    assert three_way_env.observation_space == Box(low=1., high=float('Inf'), shape=(1, 3))
     assert three_way_env.STARTING_BANK == 10
     assert three_way_env.balance == three_way_env.STARTING_BANK
     assert three_way_env.current_step == 0
     assert three_way_env.single_bet_size == 1
     assert numpy.array_equal(three_way_env.teams, numpy.array([['FCB', 'PSG'], ['MCB', 'MTA']]))
 
-@pytest.mark.parametrize("action,expected_reward", [(0, 0), (1, -1), (2, 1), (3, -1), (4, 0), (5, -2), (6, 0), (7, -1)])
+@pytest.mark.parametrize("action,expected_reward", [(0, 0), (1, -1), (2, 1), (3, -1), (4, 0),
+                                                    (5, -2), (6, 0), (7, -1)])
 def test_step(three_way_env, action, expected_reward):
     odds, reward, done, _ = three_way_env.step(action)
     assert reward == expected_reward
@@ -28,5 +29,3 @@ def test_multiple_steps(three_way_env):
     assert reward == 3
     assert three_way_env.balance == three_way_env.STARTING_BANK - 1 + 3
     assert done
-
-
--- tests/test_three_way_percentage_env.py
+++ tests/test_three_way_percentage_env.py
@@ -3,16 +3,14 @@ import numpy
 from gym.spaces import Box
 
 def test_attributes(three_way_percentage_env):
-    assert three_way_percentage_env.observation_space == Box(low=1., high=float('Inf'), shape=(3,))
+    assert three_way_percentage_env.observation_space == Box(low=1., high=float('Inf'), shape=(1, 3))
     assert three_way_percentage_env.action_space == Box(low=numpy.array([0., 0.01]),
-                                                   high=numpy.array([2 ** 3 - 0.01, 1 / 3]))
+                                                        high=numpy.array([2 ** 3 - 0.01, 1 / 3]))
     assert numpy.array_equal(three_way_percentage_env.teams, numpy.array([['FCB', 'PSG'], ['MCB', 'MTA']]))
 
-@pytest.mark.parametrize("action,expected_reward", [((0, 0.1), 0), ((1, 0.1), -1), ((2, 0.1), 1), ((3, 0.1), -1)])
-def test_step(three_way_percentage_env, action, expected_reward):
-    odds, reward, done, _ = three_way_percentage_env.step(action)
+@pytest.mark.parametrize("form,percent,expected_reward", [(0, 0.1, 0), (1, 0.1, -1), (2, 0.1, 1), (3, 0.1, -1)])
+def test_step(three_way_percentage_env, form, percent, expected_reward):
+    odds, reward, done, _ = three_way_percentage_env.step((form, percent))
     assert reward == expected_reward
     assert not done
     assert three_way_percentage_env.current_step == 1
-
-
