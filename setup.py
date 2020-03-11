52f98500d198d2b9d6868dde377757cc1c26bcbb
Ory Jonay <ojonay@infinidat.com>
2020-03-11 08:54:57 +0200

    update setup.py to use setup.cfg

--- setup.py
+++ setup.py
@@ -1,10 +1,4 @@
 from setuptools import setup, find_packages
 
-setup(name='oddsgym',
-      version='0.0.1',
-      description="A sports betting environment for OpenAI Gym.",
-      author="Ory Jonay",
-      author_email='kriegsmeister@gmail.com',
-      url='https://github.com/OryJonay/Odds-Gym',
-      packages=find_packages(),
+setup(packages=find_packages(),
       install_requires=['gym', 'more_itertools', 'pandas'])
