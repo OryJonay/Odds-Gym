248716de4bf4b63c37e109c6b22d1da71db10ae7
Ory Jonay <ojonay@infinidat.com>
2020-03-26 11:04:22 +0200

    switch theme and fix formatting error in installation page

--- conf.py
+++ conf.py
@@ -31,7 +31,8 @@ extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.mathjax',
               'sphinx.ext.napoleon',
               'sphinx.ext.todo',
-              'sphinx.ext.githubpages']
+              'sphinx.ext.githubpages',
+              'sphinx_rtd_theme']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = []
@@ -47,7 +48,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'bin', 'lib', 'include',
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_materialdesign_theme'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
--- docsrc/installation.rst
+++ docsrc/installation.rst
@@ -1,9 +1,11 @@
 Installation Guide
 ******************
+
 Dependencies
 ############
 Being an Open-AI Gym environment, gym is a major dependency. We also use pandas to make it easier to built the data for the environments.
 Also, the project support Python version 3.6, 3.7 and 3.8.
+
 Installation
 ############
 To install, simply run::
