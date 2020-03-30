1a3e78443e9153c3f2900b06fb427b33c647b2d7
Ory Jonay <ojonay@infinidat.com>
2020-03-30 09:32:33 +0300

    setup: get package version from bumpversion

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 """This module contains the packaging routine for oddsgym"""
 
 from setuptools import setup, find_packages
+from bumpversion.cli import _load_configuration
 
 setup(packages=find_packages(),
-      install_requires=['gym', 'more-itertools', 'pandas'])
+      install_requires=['gym', 'more-itertools', 'pandas'],
+      version=_load_configuration('setup.cfg', None, {})[0]['bumpversion']['current_version'])
