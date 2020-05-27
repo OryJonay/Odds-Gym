c43d2a9460235044a9e00447e81b8365c56b9891
Ory Jonay <ojonay@infinidat.com>
2020-05-27 15:37:53 +0300

    Add integration tests
    Add integration tests of the generic environments against stable-baselines
    and stable-baselines3.

--- tests/test_daily_env.py
+++ tests/test_daily_env.py
@@ -38,7 +38,7 @@ def test_attributes(daily_bets_env):
 
 def test_get_odds(daily_bets_env):
     odds = daily_bets_env.get_odds()
-    assert odds.equals(DataFrame([{'w': 1, 'l': 2}, {'w': 4, 'l': 3}], dtype=numpy.float64))
+    assert numpy.array_equal(odds, DataFrame([{'w': 1, 'l': 2}, {'w': 4, 'l': 3}], dtype=numpy.float64).values)
 
 
 @pytest.mark.parametrize("actions,bets", [(array([0, 0]), array([[0, 0], [0, 0]])),
@@ -81,7 +81,7 @@ def test_attributes_of_non_uniform(daily_bets_env_non_uniform):
 def test_get_odds_non_uniform(daily_bets_env_non_uniform, current_step, expected_odds):
     daily_bets_env_non_uniform.current_step = current_step
     odds = daily_bets_env_non_uniform.get_odds()
-    assert odds.equals(expected_odds)
+    assert numpy.array_equal(odds, expected_odds.values)
 
 
 @pytest.mark.parametrize("actions,bets", [(array([0]), zeros([3, 2])),
---
+++ tests/test_integration_with_stable_baselines.py
@@ -0,0 +1,33 @@
+import pytest
+from stable_baselines import A2C, ACKTR, DQN, DDPG, PPO1, PPO2, SAC, TD3, TRPO
+
+
+# supported RL algorithems by environment
+ENV_DICT = {'BaseOddsEnv': [A2C, ACKTR, DQN, PPO1, PPO2, TRPO],
+            'BasePercentageOddsEnv': [A2C, ACKTR, DDPG, PPO1, PPO2, SAC, TD3, TRPO],
+            'DailyOddsEnv': [A2C, ACKTR, DDPG, PPO1, PPO2, SAC, TD3, TRPO],
+            'DailyPercentageOddsEnv': [A2C, ACKTR, DDPG, PPO1, PPO2, SAC, TD3, TRPO]}
+
+
+@pytest.mark.parametrize("alg", ENV_DICT['BaseOddsEnv'])
+def test_basic_env_with_(basic_env, alg):
+    model = alg('MlpPolicy', basic_env)
+    model.learn(total_timesteps=10)
+
+
+@pytest.mark.parametrize("alg", ENV_DICT['BasePercentageOddsEnv'])
+def test_basic_percentage_env_with_(basic_percentage_env, alg):
+    model = alg('MlpPolicy', basic_percentage_env)
+    model.learn(total_timesteps=10)
+
+
+@pytest.mark.parametrize("alg", ENV_DICT['DailyOddsEnv'])
+def test_daily_env_with_(daily_bets_env, alg):
+    model = alg('MlpPolicy', daily_bets_env)
+    model.learn(total_timesteps=10)
+
+
+@pytest.mark.parametrize("alg", ENV_DICT['DailyOddsEnv'])
+def test_daily_percentage_env_with_(daily_bets_percentage_env, alg):
+    model = alg('MlpPolicy', daily_bets_percentage_env)
+    model.learn(total_timesteps=10)
