0eaad6cf36880c0f762dc3248bee7c63b0cf2150
OryJonay <OryJonay@users.noreply.github.com>
2020-06-02 19:59:24 +0300

    FootballDataDailyPercentageEnv: 3-way soccer env with odds data from www.football-data.co.uk (#8)

--- oddsgym/envs/__init__.py
+++ oddsgym/envs/__init__.py
@@ -1,8 +1,17 @@
+import gym
+
 from .base import BaseOddsEnv
 from .base_percentage import BasePercentageOddsEnv
 from .daily_bets import DailyOddsEnv, DailyPercentageOddsEnv
 from .soccer import ThreeWaySoccerOddsEnv, ThreeWaySoccerPercentageOddsEnv
 from .soccer import ThreeWaySoccerDailyOddsEnv, ThreeWaySoccerDailyPercentageOddsEnv
+from .footballdata import FootballDataDailyPercentageEnv
 
 __all__ = ['BaseOddsEnv', 'BasePercentageOddsEnv', 'DailyOddsEnv', 'DailyPercentageOddsEnv', 'ThreeWaySoccerOddsEnv',
-           'ThreeWaySoccerPercentageOddsEnv', 'ThreeWaySoccerDailyOddsEnv', 'ThreeWaySoccerDailyPercentageOddsEnv']
+           'ThreeWaySoccerPercentageOddsEnv', 'ThreeWaySoccerDailyOddsEnv', 'ThreeWaySoccerDailyPercentageOddsEnv',
+           'FootballDataDailyPercentageEnv']
+
+# register the football-data.co.uk environment, so it would be ready for usage when installing with pip
+gym.register(id='FootballDataDailyPercent-v0',
+             entry_point='oddsgym.envs:FootballDataDailyPercentageEnv',
+             max_episode_steps=365)
